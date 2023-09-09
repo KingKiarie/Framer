27a8823957226a9bdd0fd020125fd1c3df463cbd
unknown <kiarie7mbugua@gmail.com>
2023-09-09 20:27:46 +0300

    animated-pages

---
+++ src/Components/Contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Transition from "./Transition";
+
+const Contact = () => {
+  return (
+    <>
+      <h1>Contact</h1>
+    </>
+  );
+};
+
+export default Transition(Contact);
